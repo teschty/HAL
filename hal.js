312b96503d5194e09fa374a1449d24fa1d6d2c26
Jake Meyer <jakewmeyer@gmail.com>
2017-09-14 15:11:56 -0500

    added comments

--- hal.js
+++ hal.js
@@ -16,6 +16,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   
+  // Creates an array from all parts of the given command
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
